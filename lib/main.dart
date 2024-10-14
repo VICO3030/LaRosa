d217eaa0c78a5f1fbc03e767a38b183434449bbd
VICO3030 <manuelpaco234@gmail.com>
2024-10-13 23:12:04 -0400

    segundo coommit de victor

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hola Mundo  Bienvenido al xd ',
+              'Bienvenido al Mundo de Victor ',
               
             ),
             Text(
